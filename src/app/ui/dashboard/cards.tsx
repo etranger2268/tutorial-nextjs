7cdb0acf8caaf87775c027f1092e79f1b7ee23a4
etranger2268 <shun.5.2268@gmail.com>
2026-01-15 15:12:16 +0900

    chapter-08

--- src/app/ui/dashboard/cards.tsx
+++ src/app/ui/dashboard/cards.tsx
@@ -9,14 +9,14 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  return (
-    <>
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card title="Total Customers" value={numberOfCustomers} type="customers" />
-    </>
-  );
+  // return (
+  //   <>
+  //     <Card title="Collected" value={totalPaidInvoices} type="collected" />
+  //     <Card title="Pending" value={totalPendingInvoices} type="pending" />
+  //     <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+  //     <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+  //   </>
+  // );
 }
 
 export function Card({
